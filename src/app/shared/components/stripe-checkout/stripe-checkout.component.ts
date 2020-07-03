c42ec2b366a2257d8971b9680ebd222f55d5be31
Agiet Tanyingu <Agiet.Tonyingo@directenergy.com>
2020-07-03 11:48:27 -0500

    strip update

--- src/app/shared/components/stripe-checkout/stripe-checkout.component.ts
+++ src/app/shared/components/stripe-checkout/stripe-checkout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
  
-import { StripeService, Elements, Element as StripeElement, ElementsOptions } from "ngx-stripe";
+import { StripeService, Elements, Element as StripeElement, ElementsOptions, StripeCardComponent, ElementOptions } from "ngx-stripe";
 
 
 @Component({
@@ -10,64 +10,56 @@ import { StripeService, Elements, Element as StripeElement, ElementsOptions } fr
     styleUrls: ['./stripe-checkout.component.scss']
 })
 export class StripeCheckoutComponent implements OnInit {
-    elements: Elements;
-    card: StripeElement;
-
-    // optional parameters
-    elementsOptions: ElementsOptions = {
-        locale: 'es'
+    @ViewChild(StripeCardComponent) card: StripeCardComponent;
+    public cardOptions: ElementOptions = {
+        style: {
+            base: {
+                iconColor: '#666EE8',
+                color: '#31325F',
+                lineHeight: '40px',
+                fontWeight: 300,
+                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                fontSize: '1.5rem',
+                '::placeholder': {
+                    color: '#CFD7E0'
+                }
+            }
+        }
     };
 
-    stripeTest: FormGroup;
+    elementsOptions: ElementsOptions = {
+        locale: 'en'
+      };
+    stripeForm: FormGroup;
 
     constructor(
         private fb: FormBuilder,
         private stripeService: StripeService) { }
 
     ngOnInit() {
-        this.stripeTest = this.fb.group({
+        this.stripeForm = this.fb.group({
             name: ['', [Validators.required]]
         });
-        this.stripeService.elements(this.elementsOptions)
-            .subscribe(elements => {
-                this.elements = elements;
-                // Only mount the element the first time
-                if (!this.card) {
-                    this.card = this.elements.create('card', {
-                        style: {
-                            base: {
-                                iconColor: '#666EE8',
-                                color: '#31325F',
-                                lineHeight: '40px',
-                                fontWeight: 300,
-                                fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-                                fontSize: '18px',
-                                '::placeholder': {
-                                    color: '#CFD7E0'
-                                }
-                            }
-                        }
-                    });
-                    this.card.mount('#card-element');
-                }
-            });
     }
 
     buy() {
-        const name = this.stripeTest.get('name').value;
+        console.log(this.card.getCard())
+        const name = this.stripeForm.get('name').value;
         this.stripeService
-            .createToken(this.card, { name })
-            .subscribe(result => {
-                if (result.token) {
-                    // Use the token to create a charge or a customer
-                    // https://stripe.com/docs/charges
-                    console.log(result.token);
-                } else if (result.error) {
-                    // Error creating the token
-                    console.log(result.error.message);
-                }
-            });
-    }
+          .createToken(this.card.getCard(), { name })
+          .subscribe(result => {
+            if (result.token) {
+                console.log('this is the result')
+                console.log(result)
+              // Use the token to create a charge or a customer
+              // https://stripe.com/docs/charges
+              
+            } else if (result.error) {
+              // Error creating the token
+              console.log(result.error.message);
+            }
+          });
+      }
 
 }
     // @Input() public amount: number;
