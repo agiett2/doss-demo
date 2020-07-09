58414c27ebfe71556422b78500da88560ad1166f
Agiet Tanyingu <Agiet.Tonyingo@directenergy.com>
2020-07-08 22:16:37 -0500

    update email configs and app.js

--- server/app.js
+++ server/app.js
@@ -4,14 +4,18 @@ const stripeController = require('./stripe-controller');
 const emailController = require('./email-controller');
 const cors = require("cors");
 const bodyParser = require("body-parser");
+var router = app.Router();
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
-
+app.use('/api', router);
 app.listen(3000, () => console.log("Server has started on port 3000!"));
 app.get('/', (req, res) => {
     res.send(
       "<h1 style='text-align: center'>Welcome to DOSS API </h1>"
     );
   });
-app.post('/createCharge', stripeController.createCharge);
-app.post('/sendEmail', emailController.sendEmail);
+
+
+router.post('/createCharge', stripeController.createCharge);
+router.post('/sendEmail', emailController.sendEmail);
+
